b9945df5006d0ce5d043524796488b3e6a588bb4
William Castrejón <wcastrejonc17_1@unc.edu.pe>
2024-06-30 10:36:03 -0500

    se crea el proyecto

---
+++ src/main/java/com/codigo/mscastrejoncarrascohexagonal/MsCastrejoncarrascoHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.codigo.mscastrejoncarrascohexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCastrejoncarrascoHexagonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCastrejoncarrascoHexagonalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codigo/mscastrejoncarrascohexagonal/MsCastrejoncarrascoHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigo.mscastrejoncarrascohexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCastrejoncarrascoHexagonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
